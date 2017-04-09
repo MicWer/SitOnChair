5b395ceb6a1472099c91344d24e2145d4d25a219
MicWer <werek91@gmail.com>
2017-04-09 13:41:17 +0200

    added form validation in JS

--- js/script.js
+++ js/script.js
@@ -43,3 +43,70 @@ document.addEventListener("DOMContentLoaded", function () {
     prevImg.addEventListener("click", prev);
     nextImg.addEventListener("click", next);
 });
+
+var form = document.querySelector("form");
+var email = document.getElementById("email");
+var text = document.getElementById("text");
+var agree = document.getElementById("transport");
+var errorDiv = document.querySelector(".error");
+var successDiv = document.querySelector(".success");
+
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var formValidation = true;
+    var allErrors = [];
+    errorDiv.innerText = "";
+
+    var name = document.getElementById("name").value;
+
+    if (name.length >= 3) {
+        console.log("Poprawna ilość znaków imienia");
+    } else {
+        allErrors.push("Twoje imię powinno zawierać przynajmniej 3 znaki");
+        formValidation = false;
+    }
+
+    if ((email.value.indexOf("@") >= 1) && (email.value.length >= 3) && (email.value.indexOf("@") < (email.value.length - 1))) {
+        console.log("Poprawny adres e-mail");
+    } else if (email.value.indexOf("@") === 0) {
+        allErrors.push("Niepoprawny adres e-mail, wpisz nazwę przed '@'");
+        formValidation = false;
+    } else if ((email.value.indexOf("@") >= 1) && (email.value.indexOf("@") === email.value.length - 1)) {
+        allErrors.push("Niepoprawny adres e-mail, wpisz nazwę po '@'");
+        formValidation = false;
+    } else {
+        allErrors.push("Brak '@' w adresie email");
+        formValidation = false;
+    }
+
+    var text = document.getElementById("text").value;
+    if (text.length >= 1) {
+        console.log("Poprawna wiadomość");
+    } else {
+        allErrors.push("Pustych wiadomości nie czytamy.");
+        formValidation = false;
+    }
+
+    if (agree.checked) {
+        console.log("Zgoda na przetwarzanie danych");
+    } else {
+        allErrors.push("Bez zgody na przetwarzanie danych osobowych, nie jesteśmy w stanie odpowiedzieć na twoje pytania.");
+        formValidation = false;
+    }
+
+    if (formValidation) {
+        successDiv.innerText = "Spodziewaj się odpowiedzi jeszcze dziś!";
+        setTimeout(function() {
+            form.submit();
+        }, 3000);
+    } else {
+        for (var i = 0; i < allErrors.length; i++) {
+            var p = document.createElement("p");
+
+            p.innerText = allErrors[i];
+            errorDiv.appendChild(p);
+        }
+    }
+
+});
